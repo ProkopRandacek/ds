e9f7e07d8046cb3fff783f8023bfe41c4e672d06
Prokop Randacek <prokop@randacek.dev>
2022-05-29 20:35:01 +0200

    likely && unlikely macros

--- common.h
+++ common.h
@@ -1,10 +1,10 @@
 #ifndef DS_COMMON_H
 #define DS_COMMON_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 typedef uint8_t byte;
 typedef uint8_t u8;
@@ -24,15 +24,18 @@ typedef unsigned int uint;
 #define ds_max(a, b) (((a) > (b)) ? (a) : (b))
 #define ds_abs(x) ((x) < 0 ? -(x) : (x))
 
-#define ds_glue(x,y) x##y
-#define ds_glue_(x,y) x##_##y
-#define ds_glue_expanded(x,y) glue(x,y)
-#define ds_glue_expanded_(x,y) glue_(x,y)
+#define ds_glue(x, y) x##y
+#define ds_glue_(x, y) x##_##y
+#define ds_glue_expanded(x, y) glue(x, y)
+#define ds_glue_expanded_(x, y) glue_(x, y)
 
 #define clzl(x) __builtin_clzl(x)
 #define clz(x) __builtin_clz(x)
 #define ctzl(x) __builtin_ctzl(x)
 #define ctz(x) __builtin_ctz(x)
 #define popc(x) __builtin_popcount(x)
+
+#define likely(x) __builtin_expect((x), 1)
+#define unlikely(x) __builtin_expect((x), 0)
 
 #endif
