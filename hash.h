6b41367c94c4acb55fb7e9c3e63ed19b99cd1903
Prokop Randacek <prokop@randacek.dev>
2022-05-29 14:50:55 +0200

    hashing functions for memory blocks and strings

--- hash.h
+++ hash.h
@@ -1,20 +1,31 @@
 #include "common.h"
 
+// taken from https://www.ucw.cz/libucw/
 static uint hash_u32(uint x) { return 0x01008041 * x; }
-
 static uint hash_u64(u64 x) { return hash_u32((uint)x ^ (uint)(x >> 32)); }
-
-#if 0
-static uint hash_mem(void* v, size_t l) {
-  uint h = 0;
-  void* end = v + l;
-  for (; v < end; h ^= hash_u32(v))
-    ;
-}
-#endif
-
 static uint hash_pointer(void* x) {
   return ((sizeof(x) <= 4) ? hash_u32((uint)(uintptr_t)x)
                            : hash_u64((u64)(uintptr_t)x));
+}
+
+// http://www.cse.yorku.ca/~oz/hash.html - djb2
+static uint hash_str(char* str) {
+  uint hash = 5381;
+  int c;
+
+  while ((c = *str++))
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+  return hash;
+}
+
+// http://www.cse.yorku.ca/~oz/hash.html - djb2 modified
+static uint hash_mem(size_t len, char* str) {
+  uint hash = 5381;
+
+  for (size_t i = 0; i < len; i++)
+    hash = ((hash << 5) + hash) + str[i]; /* hash * 33 + c */
+
+  return hash;
 }
 
